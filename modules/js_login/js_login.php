1ce9c22d41b92afafb58194dcd1041a0c61f9c0d
Maximilian Narr <maxesstuff@live.de>
2011-04-29 16:00:42 +0200

    modules js_login and loginButton: The password for the server can be now setted in the configfile that the user doesn't has to insert it-

--- modules/js_login/js_login.php
+++ modules/js_login/js_login.php
@@ -1,30 +1,46 @@
 <?php
-class js_login extends ms_Module {
 
-    function __construct($info,$config,$lang,$mm) {
-        parent::__construct($info,$config,$lang,$mm);
+class js_login extends ms_Module
+{
+
+    function __construct($info, $config, $lang, $mm)
+    {
+        parent::__construct($info, $config, $lang, $mm);
 
         $prompt = bool2text($this->config['prompt_username']);
-        $pass_n = bool2text($this->config['have_server_pass']);
+        $pass_n = bool2text($this->config['prompt_pass']);
+        $serverpassword = $this->config['server_password'];
+
+        if ($serverpassword != null && $serverpassword != "")
+        {
+            $pass_n = "false";
+        }
+        else
+        {
+            $serverpassword = 0;
+        }
 
         $this->mManager->loadModule('jQuery');
-        $this->mManager->loadModule('js')->loadJS(s_http.'/modules/js_login/ts3_connect.js');
+        $this->mManager->loadModule('js')->loadJS(s_http . '/modules/js_login/ts3_connect.js');
 
         $prompt_serverpass = $this->lang['prompt_serverpass'];
         $prompt_nickname = $this->lang['prompt_nickname'];
 
-        if(isset($this->config['connect_host']) && $this->config['connect_host'] != $this->config['host'])
-                $this->config['host'] = $this->config['connect_host'];
+        if (isset($this->config['connect_host']) && $this->config['connect_host'] != $this->config['host'])
+            $this->config['host'] = $this->config['connect_host'];
+
         $this->mManager->loadModule('js')->loadJS("$(document).ready( function() {
                         $('.servername').click(function() {
-                                ts3_connect('".$this->config['host']."','".$this->config['vserverport']."',".$pass_n.",".$prompt.", '".$prompt_serverpass."','".$prompt_nickname."');
+                                ts3_connect('" . $this->config['host'] . "','" . $this->config['vserverport'] . "'," . $pass_n . "," . $serverpassword. "," . $prompt . ", '" . $prompt_serverpass . "','" . $prompt_nickname . "');
                                 })
-                        });",'text');
+                        });", 'text');
 
 
         $this->mManager->loadModule('style')->loadStyle('.servername {
                         cursor: pointer
-                }','text');
+                }', 'text');
     }
+
 }
+
 ?>
--- modules/js_login/ts3_connect.js
+++ modules/js_login/ts3_connect.js
@@ -1,23 +1,33 @@
 
-function ts3_connect(host,port,server_pass_needed,prompt_username, server_pass_i18n, nickname_i18n) {
+function ts3_connect(host,port,server_pass_needed, serverpass, prompt_username, server_pass_i18n, nickname_i18n) {
     uri = 'ts3server://'+ host + '?port=' + port;
+    
+    // Checks If the password should be automatically added
+    if(serverpass != "" && serverpass != null && serverpass != undefined)
+    {
+        uri = uri + '&pass=' + serverpass;
+    }
+    
+    // Checks If password should be prompted
     if(server_pass_needed == true) {
-		var pass = prompt(server_pass_i18n);
-		if(pass == "" || pass == null || pass == undefined) {
-			return;
-		}
+        var pass = prompt(server_pass_i18n);
+        if(pass == "" || pass == null || pass == undefined) {
+            return;
+        }
         uri = uri + '&pass=' + pass ;
     }
+
+    // checks If username should be prompted
     if(prompt_username == true) {
-		var name = prompt(nickname_i18n);
-		if(name == "" || name == null || name == undefined) {
-			return;
-		}
+        var name = prompt(nickname_i18n);
+        if(name == "" || name == null || name == undefined) {
+            return;
+        }
         uri = uri + '&nickname=' + name;
     }
 
     var popup = window.open(uri);
-	popup.close();
+    popup.close();
 
 
 }
