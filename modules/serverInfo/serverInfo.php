800d13517811effd04f0c4f8c9f35d8603615a1b
Maximilian Narr <maxesstuff@live.de>
2011-05-14 18:35:07 +0200

    ~ Module: serverinfo: added option in the config which defines which numberformat should be used for traffic (MiB, GiB, ...)

--- modules/serverInfo/serverInfo.php
+++ modules/serverInfo/serverInfo.php
@@ -1,34 +1,43 @@
 <?php
+
 class serverInfo extends ms_Module
 {
+
     public $html = "";
     public $use_tab = false;
 
-    function __construct($info,$config,$lang,$mm)
+    function __construct($info, $config, $lang, $mm)
     {
-        parent::__construct($info,$config,$lang,$mm);
+        parent::__construct($info, $config, $lang, $mm);
 
-        require_once s_root.'modules/serverInfo/nbbc/nbbc.php';
+        require_once s_root . 'modules/serverInfo/nbbc/nbbc.php';
 
         $this->use_tab = $this->config['use_tab'];
+        
+        $value_format = "mb";
+        
+        if($this->config['value_format'] != NULL || $this->config['value_format'] != "")
+        {
+            $value_format = $this->config['value_config'];
+        }
+                
 
         $bbparser = new BBCode();
-
         $serverinfo = $this->info['serverinfo'];
 
+        
         $welcomemsg = '';
 
         if ($serverinfo['virtualserver_welcomemessage'] == '')
-            $welcomemsg = $this->lang['no_welcome'];
-        else
-            $welcomemsg = $bbparser->Parse ($serverinfo['virtualserver_welcomemessage']);
+                $welcomemsg = $this->lang['no_welcome'];
+        else $welcomemsg = $bbparser->Parse($serverinfo['virtualserver_welcomemessage']);
 
         $this->html.='<!--- START Serverinfo -->
             <div class="serverinfo"><table width="100%">
             <tr>
-            <td width="33%"><h5>'.$this->lang['welcome_msg'].'</h5><p style="border-width:1px;border-style:dotted;">'.$welcomemsg.'</p><h5>'.$this->lang['channels'].'</h5><p><img src="'.s_http.'modules/serverInfo/img/channel.png" alt="" />'.$serverinfo['virtualserver_channelsonline'].'</p></td>
-            <td width="33%"><h5>'.$this->lang['version'].'</h5><p>'.$serverinfo['virtualserver_version'].'</p><h5>'.$this->lang['server_os'].'</h5><p>'.$serverinfo['virtualserver_platform'].'</p></td>
-            <td width="33%"><h5>'.$this->lang['connection_info'].'</h5><h6>'.$this->lang['total_sent'].'</h6><p>'.$this->get_mb($serverinfo['connection_bytes_sent_total']).' GiB</p><h6>'.$this->lang['total_received'].'</h6><p>'.$this->get_mb($serverinfo['connection_bytes_received_total']).' GiB</td>
+            <td width="33%"><h5>' . $this->lang['welcome_msg'] . '</h5><p style="border-width:1px;border-style:dotted;">' . $welcomemsg . '</p><h5>' . $this->lang['channels'] . '</h5><p><img src="' . s_http . 'modules/serverInfo/img/channel.png" alt="" />' . $serverinfo['virtualserver_channelsonline'] . '</p></td>
+            <td width="33%"><h5>' . $this->lang['version'] . '</h5><p>' . $serverinfo['virtualserver_version'] . '</p><h5>' . $this->lang['server_os'] . '</h5><p>' . $serverinfo['virtualserver_platform'] . '</p></td>
+            <td width="33%"><h5>' . $this->lang['connection_info'] . '</h5><h6>' . $this->lang['total_sent'] . '</h6><p>' . $this->get_value($serverinfo['connection_bytes_sent_total'], $value_format) . '</p><h6>' . $this->lang['total_received'] . '</h6><p>' . $this->get_value($serverinfo['connection_bytes_received_total'], $value_format) . '</td>
             </tr>
             </table></div>
             <!--- END Serverinfo -->';
@@ -70,25 +79,44 @@ class serverInfo extends ms_Module
                 vertical-align:top;
             }
             /* END Serverinfo */
-            ',"text");
-        
+            ',
+                "text");
+
         if ($this->use_tab == true)
         {
-                    
-                    $this->mManager->loadModule("infoTab")->addTab($this->lang['serverinfo_tab_title'], $this->html);
+
+            $this->mManager->loadModule("infoTab")->addTab($this->lang['serverinfo_tab_title'],
+                    $this->html);
         }
     }
 
-    //Konvertiert Byte zu Mebibyte
-    function get_mb($input)
+    //Konvertiert Byte zu anderen Größen
+    function get_value($input, $format)
     {
-        return number_format($input/(1024*1024*1024), 1);
+        switch ($format)
+        {
+            case "b":
+                return(number_format($input, 1) . " Bytes");
+                break;
+            case "kb":
+                return(number_format($input/(1024), 1)." KiB");
+                break;
+            case "mb":
+                return(number_format($input/(1024*1024), 1)." MiB");
+                break;
+            case "gb":
+                return(number_format($input/(1024*1024*1024), 1)." GiB");
+                break;
+            case "tb":
+                return(number_format($input/(1024*1024*1024*1024), 1)." TiB");
+                break;
+        }
     }
 
     public function getFooter()
     {
-       
-        if($this->use_tab == false)
+
+        if ($this->use_tab == false)
         {
             return $this->html;
         }
@@ -97,5 +125,7 @@ class serverInfo extends ms_Module
             return '';
         }
     }
+
 }
+
 ?>
