9e2056ae8e152d3b1b8cda60a754ad8a114f3bb5
Maximilian Narr <maxesstuff@live.de>
2011-06-19 15:29:07 +0200

    * Fixed bug that flags weren't shown in the infoDialog

--- modules/serverInfo/serverInfo.php
+++ modules/serverInfo/serverInfo.php
@@ -35,7 +35,7 @@ class serverInfo extends ms_Module
         $this->html.='<!--- START Serverinfo -->
             <div class="serverinfo"><table width="100%">
             <tr>
-            <td width="33%"><h5>' . $this->lang['welcome_msg'] . '</h5><p style="border-width:1px;border-style:dotted;">' . $welcomemsg . '</p><h5>' . $this->lang['channels'] . '</h5><p><span class="channelimage normal-channel">&nbsp;</span>' . $serverinfo['virtualserver_channelsonline'] . '</p></td>
+            <td width="33%"><h5>' . $this->lang['welcome_msg'] . '</h5><p style="border-width:1px;border-style:dotted; padding: 2px;">' . $welcomemsg . '</p><h5>' . $this->lang['channels'] . '</h5><p><span class="channelimage normal-channel">&nbsp;</span>' . $serverinfo['virtualserver_channelsonline'] . '</p></td>
             <td width="33%"><h5>' . $this->lang['version'] . '</h5><p>' . $serverinfo['virtualserver_version'] . '</p><h5>' . $this->lang['server_os'] . '</h5><p>' . $serverinfo['virtualserver_platform'] . '</p></td>
             <td width="33%"><h5>' . $this->lang['connection_info'] . '</h5><h6>' . $this->lang['total_sent'] . '</h6><p>' . $this->get_value($serverinfo['connection_bytes_sent_total'],
                         $value_format) . '</p><h6>' . $this->lang['total_received'] . '</h6><p>' . $this->get_value($serverinfo['connection_bytes_received_total'],
