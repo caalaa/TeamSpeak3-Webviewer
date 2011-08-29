51455ac682bf2a22b51be6af6979d3cfe9f5c1c1
Maximilian Narr <maxesstuff@live.de>
2011-08-29 23:20:23 +0200

    updated german and english translation

--- l10n/en_US/lang.php
+++ l10n/en_US/lang.php
@@ -1,7 +1,7 @@
 <?php
 $l10_land = array();
 $l10_lang['lang'] = __('English');
-$l10_lang['version'] = 0.9;
+$l10_lang['version'] = "1.0";
 $l10_lang['author'] = 'Maximilian Narr';
 $l10_lang['url'] = 'http://maxesstuff.de';
 ?>
--- modules/userBar/userBar.php
+++ modules/userBar/userBar.php
@@ -1,36 +1,39 @@
 <?php
+
 class userBar extends ms_Module
 {
+
     public $max_clients = "";
     public $clients_online = "";
     protected $jsModule;
     protected $styleModule;
-    
-    public function init() {
-       
+
+    public function init()
+    {
+
         $this->mManager->loadModule("jQueryUI");
         $this->jsModule = $this->mManager->loadModule('js');
         $this->styleModule = $this->mManager->loadModule('style');
     }
-    
+
     function onInfoLoaded()
     {
-        
-        
+
+
         //L10N
-         setL10n($this->config['language'], "mssetL10n-tsv-userBar");
-        
-        
+        setL10n($this->config['language'], "ms-tsv-userBar");
+
+
         $this->max_clients = $this->info['serverinfo']['virtualserver_maxclients'];
         $this->clients_online = $this->info['serverinfo']['virtualserver_clientsonline'] - $this->info['serverinfo']['virtualserver_queryclientsonline'];
 
         $per_cent = $this->clients_online / $this->max_clients * 100;
         $this->jsModule->loadJS('$(document).ready(function() {
                                                    $("#userBar").progressbar({
-                                                   value: '.$per_cent.'
+                                                   value: ' . $per_cent . '
                                                    });
-                                                  });',"text");
-        
+                                                  });', "text");
+
         $this->styleModule->loadStyle('#userBar
             {
                 margin-bottom:5px;
@@ -39,9 +42,11 @@ class userBar extends ms_Module
 
     public function getHeader()
     {
-         setL10n($this->config['language'], "mssetL10n-tsv-userBar");
-        return('<p style="font-family: sans-serif; font-size: small;">'.$this->clients_online.' '.__('of').' '.$this->max_clients.' '.__('are currently online').'</p>
+        setL10n($this->config['language'], "mssetL10n-tsv-userBar");
+        return('<p style="font-family: sans-serif; font-size: small;">' . $this->clients_online . ' ' . __('of') . ' ' . $this->max_clients . ' ' . __('clients are currently online') . '</p>
                 <div id="userBar" style="height:15px; width:100%; margin-top:10px;"></div>');
     }
+
 }
+
 ?>
