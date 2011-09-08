9464fd739316047e0936bea278ac753c3b2cb166
Maximilian Narr <maxesstuff@live.de>
2011-09-08 09:10:45 +0200

    updated russian translation

--- l10n/ru_RU/lang.php
+++ l10n/ru_RU/lang.php
@@ -1,7 +1,7 @@
 <?php
 $l10_lang = array();
 $l10_lang['lang'] = __('Russian');
-$l10_lang['version'] = 0.9;
+$l10_lang['version'] = '1.0';
 $l10_lang['author'] = 'crysea';
 $l10_lang['url'] = 'http://vk.com/grustniy';
 ?>
