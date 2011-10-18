22f1cddb34f7b9b94157186bba0434f3f77c0466
Maximilian Narr <maxesstuff@live.de>
2011-10-18 16:49:57 +0200

    updated cron file

--- modules/stats/cron.php
+++ modules/stats/cron.php
@@ -8,12 +8,10 @@ $configFiles = "devmx";
 
 // If you want to update several configfiles (Comment out inappropriate)
 //$configFiles = array("config1", "config2");
-
 // ************************************************************ \\
 // END CONFIGURATION DON'T EDIT ANYTHING BEYOND THIS LINE       \\
 // ************************************************************ \\
 define('s_root', "../../");
-
 
 require_once '../../core/teamspeak/TSQuery.class.php';
 
@@ -43,37 +41,49 @@ exit;
  * @since 1.1
  */
 function updateStatsFile($configfile)
-{  
+{
     $configfile = str_replace(".xml", "", $configfile);
     $xml = simplexml_load_file("../../config/$configfile.xml");
 
     $customDir = s_root . 'cache/' . $xml->host . $xml->queryport . '/' . $xml->vserverport . '/';
-    
+
     try
     {
         require_once 'php/utils.php';
 
         if (needNewEntry($configfile, $customDir))
         {
-            $query = new TSQuery((string)$xml->host, (string)$xml->queryport);
+            $query = new TSQuery((string) $xml->host, (string) $xml->queryport);
 
-            if (isset($xml->login_needed) && (bool)$xml->login_needed)
+            if (isset($xml->login_needed) && (bool) $xml->login_needed)
             {
-                $query->login((string)$xml->username, (string)$xml->password);
+                $login = $query->login((string) $xml->username, (string) $xml->password);
+                if (isset($login['error']['msg']) && trim($login['error']['msg']) != "ok")
+                {
+                    $query->quit();
+                    throw new Exception("Failed to login to TS Query:  " . $login['error']['msg']);
+                }
             }
 
-            $query->use_by_port((string)$xml->vserverport);
+            $use = $query->use_by_port((string) $xml->vserverport);
 
-            $serverinfo = $query->serverinfo() or exit;
-            $serverinfo = $serverinfo['return'];
-            
-            if($serverinfo['error']['message'] != "ok")
+            if (isset($serverinfo['error']['msg']) && trim($use['error']['msg']) != "ok")
             {
                 $query->quit();
+                throw new Exception("Failed to select virtualserver: " . $use['error']['msg']);
             }
-            
+
+
+            $serverinfo = $query->serverinfo();
+
+            if (isset($serverinfo['error']['msg']) && trim($serverinfo['error']['msg']) != "ok")
+            {
+                $query->quit();
+                throw new Exception("Failed to retrieve serverinfo: " . $serverinfo['error']['msg']);
+            }
+
             $query->quit();
-            
+
             if (isset($serverinfo['virtualserver_clientsonline']) && isset($serverinfo['virtualserver_queryclientsonline']))
             {
                 addEntry((int) $serverinfo['virtualserver_clientsonline'] - (int) $serverinfo['virtualserver_queryclientsonline'], $configfile, $customDir);
@@ -82,6 +92,7 @@ function updateStatsFile($configfile)
     }
     catch (Exception $ex)
     {
+        echo "devMX Webviewer Fatal Error: " . $ex->getMessage();
     }
 }
 ?>
--- modules/stats/cron.sh
+++ modules/stats/cron.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 cd /var/www/testing/maxe/tswebviewer/modules/stats
 php cron.php
+exit 0
