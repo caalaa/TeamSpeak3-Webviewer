12b28c64f71173f5e20237da048d8e5cbef51d71
Maximilian Narr <maxesstuff@live.de>
2011-10-20 15:43:00 +0200

    updated cron file

--- modules/stats/cron.php
+++ modules/stats/cron.php
@@ -1,5 +1,21 @@
 #!/usr/bin/php -f
 <?php
+/**
+ * This file is part of TeamSpeak3 Webviewer.
+ *
+ * TeamSpeak3 Webviewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TeamSpeak3 Webviewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TeamSpeak3 Webviewer. If not, see http://www.gnu.org/licenses/.
+ */
 // ************************************************************ \\
 // EDIT THE FOLLOWING LINES THAT THEY FIT TO YOUR CONFIGURATION \\
 // ************************************************************ \\
@@ -45,7 +61,7 @@ function updateStatsFile($configfile)
     $configfile = str_replace(".xml", "", $configfile);
     $xml = simplexml_load_file("../../config/$configfile.xml");
 
-    $customDir = s_root . 'cache/' . $xml->host . $xml->queryport . '/' . $xml->vserverport . '/';
+    $customDir = '../../cache/' . $xml->host . $xml->queryport . '/' . $xml->vserverport . '/';
 
     try
     {
