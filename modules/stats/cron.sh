0592c28f65a5616b237bf3f07603175c4d3254c1
Maximilian Narr <maxesstuff@live.de>
2011-10-30 14:47:04 +0100

    updated cron file

--- modules/stats/cron.sh
+++ modules/stats/cron.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+
+# ++++++++++++++++++ EDIT APPROPRIATE TO YOUR CONFIGURATION +++++++++++++++
+# Adapt the path that it fits to the location of the stats module folder ++
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 cd /var/www/testing/maxe/tswebviewer/modules/stats
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 php cron.php
 exit 0
