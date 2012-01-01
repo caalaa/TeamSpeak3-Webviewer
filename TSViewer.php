8da19dc574f3a6f0b9dbc6d003fa8e967597d567
Maximilian Narr <maxesstuff@live.de>
2012-01-01 19:07:29 +0100

    updated translations and version information

--- TSViewer.php
+++ TSViewer.php
@@ -32,7 +32,7 @@ session_name('ms_ts3Viewer');
 session_start();
 
 // Defines Current Version
-define('version', "1.2");
+define('version', "1.3");
 
 // **************************************************************** \\
 // STARTING EDITABLE CONTENT                                        \\
