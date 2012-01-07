668e60a4443efab84a91375109b223261dc76006
Maximilian Narr <maxesstuff@live.de>
2012-01-07 19:19:52 +0100

    v1.3.1

--- TSViewer.php
+++ TSViewer.php
@@ -32,7 +32,7 @@ session_name('ms_ts3Viewer');
 session_start();
 
 // Defines Current Version
-define('version', "1.3");
+define('version', "1.3.1");
 
 // **************************************************************** \\
 // STARTING EDITABLE CONTENT                                        \\
