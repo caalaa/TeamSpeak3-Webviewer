da8d29090cfb1588f6fb02d1205d92571b5d52d0
Maximilian Narr <maxesstuff@live.de>
2012-06-30 17:41:57 +0200

    - Issue #97: Removed global configuration from installscript (Deprecated). Closes #97

--- install/html/xmledit.php
+++ install/html/xmledit.php
@@ -15,7 +15,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with devMX TeamSpeak3 Webviewer.  If not, see <http://www.gnu.org/licenses/>.
- */ 
+ */
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
@@ -24,12 +24,6 @@
     <head>
         <title>TeamSpeak3 Webviewer - Installation</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        
-        <!-- Jquery -->
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-
-        <!-- Jquery UI -->
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.19/jquery-ui.min.js"></script>
 
         <link rel="stylesheet" href="../../libraries/grayified/grayified-jquery-ui-1.0.css" type="text/css" />
         <link rel="stylesheet" href="../../libraries/codemirror/lib/codemirror.css" type="text/css">
@@ -39,36 +33,21 @@
 
         <!-- Style -->
         <link rel="stylesheet" href="../css/xmledit.css" type="text/css">
-
-        <script type="text/javascript">
-            $(document).ready(function(){
-                $("input:submit").button();
-                $("#tabs").tabs({fx: { height: "toggle", duration: "slow" }});
-                $(".warning, .info, .alert").delay(500).fadeIn(500);
-            });
-        </script>
     </head>
     <body>
         <?php echo($msERRWAR); ?>
         <div id="xmledit">
             <div class="ui-state-highlight ui-corner-all" style="margin-top: 20px; padding: 0 .7em; margin-bottom: 10px;"> 
                 <p><span class="ui-icon ui-icon-info" style="float: left; margin-right: .3em;"></span>
-                    <?php __e('You can edit the global and the local configfile of the module here. If you have finished just press the button. The local configfile overrides all configparameters set in the global one. Set \'none\' in the local config, if you want to use the parameter of the global config') ?></p>
+                    <?php __e('You can edit the configfile of the module here. If you have finished just press the button. The values you set here will override the default ones.') ?></p>
                 <p><span class="ui-icon ui-icon-info" style="float: left; margin-right: .3em;"></span>
-                    <a href="http://devmx.de/en/software/teamspeak3-webviewer/dokumentation/modules/<?php echo(strtolower($module)) ?>" target="_blank"><?php __e('Learn more about this module') ?></a></p>
+                    <a href="http://docs.devmx.de/teamspeak3-webviewer/modules/" target="_blank"><?php __e('Learn more about this module') ?></a></p>
             </div>
 
             <div id="tabs">
                 <ul>
-                    <li><a href="#global"><?php __e('Global config') ?></a></li>
                     <li><a href="#local"><?php __e('Local config') ?></a></li>
                 </ul>
-                <div id="global">
-                    <form method="POST" action="xmledit.php?action=submit&type=global&module=<?php echo($module) ?>">
-                        <textarea id="global-config" name="xml"><?php echo($globalConfig) ?></textarea>
-                        <p><input type="submit" value="<?php __e('save configfile') ?>"/></p>
-                    </form>
-                </div>
                 <div id="local">
                     <form method="POST" action="xmledit.php?action=submit&type=local&module=<?php echo($module) ?>">
                         <textarea id="local-config" name="xml"><?php echo($localConfig) ?></textarea>
@@ -78,15 +57,26 @@
             </div>
 
         </div>
+
+        <!-- Jquery -->
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <!-- Jquery UI -->
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $("input:submit").button();
+                $("#tabs").tabs({fx: { height: "toggle", duration: "slow" }});
+                $(".warning, .info, .alert").delay(500).fadeIn(500);
+            });
+        </script>
         <script src="../../libraries/codemirror/lib/codemirror.js" type="text/javascript"></script>
         <script src="../../libraries/codemirror/mode/xml/xml.js" type="text/javascript"></script>
         <script type="text/javascript">              
             $(document).ready(function(){                        
-                var globalEditor = CodeMirror.fromTextArea(document.getElementById("global-config"), {mode: {name: "xml", alignCDATA: true},   lineNumbers: true });
                 var localEditor = CodeMirror.fromTextArea(document.getElementById("local-config"), {mode: {name: "xml", alignCDATA: true},   lineNumbers: true });
                 
                 $( "#tabs" ).bind( "tabsshow", function(event, ui) {
-                    globalEditor.refresh();
                     localEditor.refresh();
                 });   
             });        
