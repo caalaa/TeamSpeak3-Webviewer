b003dd0a4137024743b6d42a5d7659dbc21b6389
Maximilian Narr <Maxe94@live.de>
2012-07-09 18:23:11 +0200

    increased version number

--- bootstrap.php
+++ bootstrap.php
@@ -20,7 +20,7 @@
 session_name('ms_ts3Viewer');
 session_start();
 // Defines Current Version
-define('version', "1.4.1");
+define('version', "1.4.2");
 
 // **************************************************************** \\
 // STARTING EDITABLE CONTENT                                        \\
